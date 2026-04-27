3373930d479105f8af0c4742352582aad663d0f2
iskyrainy <wangnan5117@gmail.com>
2026-04-27 23:54:58 +0800

    fix: keymaps conflict

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -1,13 +1,11 @@
 return {
   {
     "AstroNvim/astrocore",
-    opts = {
-      mappings = {
-        v = {
-          ["J"] = { ":m '>+1<CR>gv=gv", desc = "Move line down" },
-          ["K"] = { ":m '<-2<CR>gv=gv", desc = "Move line up" },
-        },
-      },
-    },
+    opts = function(_, opts)
+      opts.mappings.v = vim.tbl_extend("force", opts.mappings.v or {}, {
+        J = { ":m '>+1<CR>gv=gv", desc = "Move line down" },
+        K = { ":m '<-2<CR>gv=gv", desc = "Move line up" },
+      })
+    end,
   },
 }
