d99deab3de4bd3c264b14a8ed85f544f32fdeab9
iskyrainy <wangnan5117@gmail.com>
2026-06-25 16:15:37 +0800

    feat: add keymap: search selected block

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -3,8 +3,9 @@ return {
     "AstroNvim/astrocore",
     opts = function(_, opts)
       opts.mappings.v = vim.tbl_extend("force", opts.mappings.v or {}, {
-        J = { ":m '>+1<CR>gv=gv", desc = "Move line down" },
-        K = { ":m '<-2<CR>gv=gv", desc = "Move line up" },
+        ["J"] = { ":m '>+1<CR>gv=gv", desc = "Move line down" },
+        ["K"] = { ":m '<-2<CR>gv=gv", desc = "Move line up" },
+        ["//"] = { 'y/<c-r>"<cr>', desc = "Search selected block" },
       })
     end,
   },
